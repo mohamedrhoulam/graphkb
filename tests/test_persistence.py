4814ac0a200a46a43875fac2fffed8c618bc4d9b
Mohamed Rhoulam <rhoulamed@gmail.com>
2025-02-22 14:05:34 +0100

    update test_persistence

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -1,10 +1,16 @@
 from graphkb.persistence.json_store import JSONStore
 from graphkb.core.graph import GraphKnowledgeBase
+import networkx as nx
 
 
 def test_save_load():
     gkb = GraphKnowledgeBase()
     store = JSONStore("data/test.json")
-    store.save(gkb)
-    loaded_gkb = store.load()
+
+    data = nx.node_link_data(gkb.graph, edges="links")
+    store.save(data)
+
+    loaded_data = store.load()
+    loaded_gkb = nx.node_link_graph(loaded_data, edges="links")
+
     assert isinstance(loaded_gkb, GraphKnowledgeBase)
