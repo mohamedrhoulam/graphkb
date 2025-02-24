f1f3ffb2b4c2f14a7f7079826e57667c0d9fbb41
Mohamed Rhoulam <rhoulamed@gmail.com>
2025-02-24 14:04:32 +0000

    add transitive lookups

--- graphkb/core/queries.py
+++ graphkb/core/queries.py
@@ -1,8 +1,20 @@
+import networkx as nx
+
 from graphkb.core.graph import GraphKnowledgeBase
+
 
 class Queries:
     def __init__(self, graph: "GraphKnowledgeBase"):
         self.graph = graph.graph
 
     def find_neighbors(self, node_id: str):
-        return list(self.graph.neighbors(node_id))
+        if node_id in self.graph.nodes:
+            return list(self.graph.neighbors(node_id))
+        else:
+            raise ValueError(f"Node {node_id} does not exist")
+
+    def transitive_lookup(self, node_id: str):
+        if node_id in self.graph.nodes:
+            return list(nx.descendants(self.graph, node_id))
+        else:
+            raise ValueError(f"Node {node_id} does not exist")
